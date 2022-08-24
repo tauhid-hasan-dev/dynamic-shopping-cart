0f0dfed75810620b1d3e514ccdf4b67fa0664758
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-08-24 18:51:25 +0300

    first commit

---
+++ product.js
@@ -0,0 +1,35 @@
+const busObject = {
+    name: 'Bus',
+    price: 200000,
+    Brand: 'Man',
+    image: 'https://images.unsplash.com/photo-1588811363284-445a2f3f6a51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJ1c3xlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60'
+}
+const carObject = {
+    name: 'Car',
+    price: 1500000,
+    Brand: 'BMW',
+    image: 'https://images.unsplash.com/photo-1643789962636-eb6e0c07ddf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGNhciUyMGJtd3xlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60'
+}
+const bikeObject = {
+    name: 'Bike',
+    price: 5550000,
+    Brand: 'Honda',
+    image: 'https://images.unsplash.com/photo-1572746965401-cb4df8f9fa79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW90b3JjeWNsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60'
+}
+
+const vanObject = {
+    name: 'Van',
+    price: 5550000,
+    Brand: 'Audi',
+    image: 'https://images.unsplash.com/photo-1652960795621-c3a86e315010?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNhcmF2YW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+}
+
+const arrOfProducts = [carObject, bikeObject, vanObject, busObject];
+
+function dynamicItem(objArr) {
+    for (let product of objArr){
+        
+    }
+}
+
+dynamicItem(arrOfProducts)
